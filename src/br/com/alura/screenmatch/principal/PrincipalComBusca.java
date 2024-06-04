fd6b7aa4be395cb3327c5fc882e045dbb0f8d7dc
mhchacon <mariana.chacon@ufrpe.br>
2024-06-04 19:52:34 -0300

    add while

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -7,6 +7,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.FieldNamingPolicy;
@@ -20,8 +22,19 @@ import br.com.alura.screenmatch.modelos.TituloOmdb;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
+        String busca = "";
+        
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
+
+        while(true){
+        
         System.out.println("Digite um filme abaixo: ");
         var buscar = leitura.nextLine();
+        if(buscar.equalsIgnoreCase("sair")){
+            break;
+        }
+        
         String endereco = "https://www.omdbapi.com/?t=" + buscar.replace(" " , "+") + "&apikey=6608c387";
 
 
@@ -36,7 +49,8 @@ public class PrincipalComBusca {
             String json = response.body();
             System.out.println(json);
             
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            
+            
             TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
             System.out.println(meuTituloOmdb);
             
@@ -45,9 +59,7 @@ public class PrincipalComBusca {
             System.out.println("Titulo convertido: ");
             System.out.println(meuTitulo);
 
-            FileWriter escrita = new FileWriter("filmes.txt");
-            escrita.write(meuTitulo.toString());
-            escrita.close();
+            titulos.add(meuTitulo);
 
             
         }catch(NumberFormatException e){
@@ -58,8 +70,17 @@ public class PrincipalComBusca {
         }catch(ErroConversaoAnoException e){
             System.out.println(e.getMessage());
         }
+    }
+            System.out.println(titulos); 
+            try(FileWriter escrita = new FileWriter("filmes.json")){
+                escrita.write(gson.toJson(titulos));
+            }catch (IOException e){
+                System.out.println("erro ao escrever no arquivo: ");
+                System.out.println(e.getMessage());
+            }
             
             System.out.println("O programa finalizou corretamente!");
-
+            
     }
+    
 }
